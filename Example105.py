dd8076b6338079fd74c0c3dc7a7ab7db86c8d150
SomeoneSlicedMyEyes <t_jumashev@kbtu.kz>
2024-01-27 12:40:39 +0600

    Examples List Comprehension

---
+++ Example105.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in range(10)]
+print(newlist)
---
+++ Example106.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in range(10) if x < 5]
+print(newlist)
